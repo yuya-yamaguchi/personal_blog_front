63a3b006ab96c2f9798ad3f5b2e4cd3caf33375c
yuya-yamaguchi <nsy1023n@gmail.com>
2022-01-09 20:44:53 +0900

    front first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:16.13.1-alpine
+
+ARG WORKDIR
+ARG CONTAINER_PORT
+
+ENV HOME=/${WORKDIR} \
+    LANG=C.UTF-8 \
+    TZ=Asia/Tokyo \
+    HOST=0.0.0.0 \
+    API_URL=${API_URL}
+
+WORKDIR ${HOME}
+
+EXPOSE ${CONTAINER_PORT}
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.$get('/api/v1/hello')
+        .then((res) => {
+          console.log(res)
+          this.msgs.push(res)
+        })
+    }
+  }
+}
+</script>
